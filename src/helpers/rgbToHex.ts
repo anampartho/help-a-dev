1ca8824683e6506ea4ed6c57b64709d2c6298b06
Tashrik Anam <anampartho31@gmail.com>
2023-09-13 04:18:37 +0600

    Add support for alpha value

--- src/helpers/rgbToHex.ts
+++ src/helpers/rgbToHex.ts
@@ -4,14 +4,23 @@ export function rgbToHex(rgb: string): string {
   // rgb(100%, 100%, 100%) and rgb(100% 100% 100%)
   const seperator = rgb.indexOf(",") > -1 ? "," : " ";
 
-  // Convert the color string to an array
-  const rgbArray = rgb.substring(4).split(")")[0].split(seperator);
+  // Alpha value
+  let alpha;
 
   // Array containing hex values converted from the rgbArray
   const hexArray: string[] = [];
 
+  // Check if it is rgba
+  const isRgba = rgb.indexOf("rgba") > -1;
+
+  // Convert the color string to an array
+  const rgbArray = rgb
+    .substring(isRgba ? 5 : 4)
+    .split(")")[0]
+    .split(seperator);
+
   // Convert rgb values from rgbArray to hex values and add it to the hexArray
-  rgbArray.forEach((colorValue) => {
+  rgbArray.forEach((colorValue, index) => {
     let hexValue = colorValue;
 
     // Convert the percentage value to proper rgb value
@@ -24,7 +33,13 @@ export function rgbToHex(rgb: string): string {
       );
     }
 
-    // Conver the number to hex string
+    // If the index is 3, this is the alpha value, multiply it with 255 and round
+    // So, alpha value of 0.4 becomes -> 102
+    if (index == 3) {
+      hexValue = String(Math.round(+hexValue * 255));
+    }
+
+    // Conver hexValue to a number and then to a hex string
     hexValue = (+hexValue).toString(16);
 
     // If only one hex value is present, then add a leading 0
