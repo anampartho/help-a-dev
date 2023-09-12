c7ca8086a174d92fe2459f295681ed223e5307a1
Tashrik Anam <anampartho31@gmail.com>
2023-09-12 18:33:58 +0600

    Update button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { IButton } from "@/utils/interfaces";
+import { Button as FlowButton, Spinner } from "flowbite-react";
 
 const Button = ({ children, onClick = () => {} }: IButton) => {
   return (
-    <button
-      className="text-xl px-4 py-[10px] rounded-lg bg-[#7F56D9] hover:bg-[#6941C6] text-white transition-[background]"
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <FlowButton onClick={onClick}>
+      <Spinner />
+      <span className="pl-3">{children}</span>
+    </FlowButton>
   );
 };
 
