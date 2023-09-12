ae8dad94ffc1c8213aeb0b0f52c5f1f4508c76d3
Tashrik Anam <anampartho31@gmail.com>
2023-09-13 04:43:26 +0600

    Fix for rgba value

--- src/helpers/rgbToHex.ts
+++ src/helpers/rgbToHex.ts
@@ -1,7 +1,15 @@
 export function rgbToHex(rgb: string): string {
   // Set seperator based on the rgb string provided
-  // checks for rgb(255, 255, 255), rgb(255 255 255),
-  // rgb(100%, 100%, 100%) and rgb(100% 100% 100%)
+  // checks for both rgb and rgba string.
+  // Example:
+  // 1. rgb(255, 255, 255)
+  // 2. rgba(255, 255, 255, 1)
+  // 3. rgb(255 255 255)
+  // 4. rgba(255 255 255 / 1)
+  // 5. rgb(100%, 100%, 100%)
+  // 6. rgba(100%, 100%, 100%, 100%)
+  // 7. rgb(100% 100% 100%)
+  // 8. rgba(100% 100% 100% / 100%)
   const seperator = rgb.indexOf(",") > -1 ? "," : " ";
 
   // Array containing hex values converted from the rgbArray
@@ -9,6 +17,11 @@ export function rgbToHex(rgb: string): string {
 
   // Check if it is rgba
   const isRgba = rgb.indexOf("rgba") > -1;
+
+  // Remove slash from string if present
+  // make sure to replace the space before it as well or we will have an
+  // empty string inside the rgbArray
+  rgb = rgb.replace(" /", "");
 
   // Convert the color string to an array
   const rgbArray = rgb
@@ -18,21 +31,28 @@ export function rgbToHex(rgb: string): string {
 
   // Convert rgb values from rgbArray to hex values and add it to the hexArray
   rgbArray.forEach((colorValue, index) => {
-    let hexValue = colorValue;
+    let trimmedColorValue = colorValue.trim();
+    let hexValue = trimmedColorValue;
 
     // Convert the percentage value to proper rgb value
     // so 100% becomes 255 => (100 / 100) * 255
-    if (colorValue.indexOf("%") > -1) {
+    if (trimmedColorValue.indexOf("%") > -1) {
       hexValue = String(
         Math.round(
-          (+colorValue.substring(0, colorValue.length - 1) / 100) * 255
+          (+trimmedColorValue.substring(0, trimmedColorValue.length - 1) /
+            100) *
+            255
         )
       );
     }
 
-    // If the index is 3, this is the alpha value, multiply it with 255 and round
+    // If the index is 3, this is the alpha value,
+    // check if it is less than or equal to 1 becuase,
+    // if percentage format is used then rather than being 0.9 it will be 90% and
+    // we don't want to do any rounding as it is already rounded.
+    // Then multiply it with 255 and then round the value
     // So, alpha value of 0.4 becomes -> 102
-    if (index == 3) {
+    if (index == 3 && +hexValue <= 1) {
       hexValue = String(Math.round(+hexValue * 255));
     }
 
